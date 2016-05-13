d5b0df74d3eb84b6b862592727d222044058fe7a
Grant Tepper <grant.tepper@gmail.com>
2016-05-13 03:19:17 -0700

    styling setup and images

--- web/app/themes/rmlg/templates/header.php
+++ web/app/themes/rmlg/templates/header.php
@@ -1,12 +1,29 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
+    <div class="container">
+        <nav class="navbar navbar-defualt navbar-fixed-top visible-xs mobile">
+            <a class="brand" href="<?= esc_url(home_url('/')); ?>">
+                <img class="img-responsive logo" alt="<?php bloginfo('name'); ?>" src="<?= get_stylesheet_directory_uri();?>/dist/images/logo.png"/>
+                <span class="logo-text"><?php bloginfo('name'); ?></span>
+            </a>
+            <div class="clearfix"></div>
+            <?php
+            if (has_nav_menu('primary_navigation')) :
+                echo '<div class="cover"></div>';
+                wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav navbar-lower']);
+            endif;
+            ?>
+        </nav>
+
+        <nav class="navbar navbar-defualt navbar-fixed-top hidden-xs">
+            <a class="brand" href="<?= esc_url(home_url('/')); ?>">
+                <img class="img-responsive logo" alt="<?php bloginfo('name'); ?>" src="<?= get_stylesheet_directory_uri();?>/dist/images/logo.png"/>
+                <span class="logo-text"><?php bloginfo('name'); ?></span>
+            </a>
+            <?php
+            if (has_nav_menu('primary_navigation')) :
+                wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav navbar-right']);
+            endif;
+            ?>
+        </nav>
+    </div>
 </header>
