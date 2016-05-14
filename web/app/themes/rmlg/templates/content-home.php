87013d2968001b0da5d92410bf9f1caddb260c5d
Grant Tepper <grant.tepper@gmail.com>
2016-05-14 15:12:46 -0700

    updates more styles and small thigns

--- web/app/themes/rmlg/templates/content-home.php
+++ web/app/themes/rmlg/templates/content-home.php
@@ -1,9 +1,9 @@
 <div class="container-fluid home">
     <section class="jumbotron">
         <div class="ghost"></div>
-        <h3>The legion shall be many; and the legion shall be free.</h3>
+        <h3>The legion shall be many; the legion shall be free.</h3>
     </section>
-    <section class="about">
+    <section class="about-us">
     </section>
     <section class="membership">
         <div class="ghost"></div>
--- web/app/themes/rmlg/templates/content-page.php
+++ web/app/themes/rmlg/templates/content-page.php
@@ -1,2 +1,16 @@
-<?php the_content(); ?>
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+<?php
+use Roots\Sage\Titles;
+
+$feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-6">
+            <h1><?= Titles\title(); ?></h1>
+            <?php the_content(); ?>
+        </div>
+        <div class="col-sm-6 image-row" style="background: url('<?= $feat_image ?>') no-repeat center center">
+        </div>
+    </div>
+</div>
