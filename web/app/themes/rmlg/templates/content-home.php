d1c3c9f7d864ab8eac309f9b34ea4d9487e2016b
Grant Tepper <grant.tepper@gmail.com>
2016-06-18 20:16:34 -0700

    removes unneeded templates

--- web/app/themes/rmlg/templates/content-home.php
+++ web/app/themes/rmlg/templates/content-home.php
@@ -7,10 +7,13 @@ $args = [
     'post_status' => 'private'
 ];
 
+
 function packagePage(Array &$sections, $page){
+    $id = $page->ID;
     array_push($sections, [
         $page,
-        wp_get_attachment_url( get_post_thumbnail_id($page->ID))
+        wp_get_attachment_url( get_post_thumbnail_id($id)),
+        get_field('bottom_copy', $id)
     ]);
 }
 
@@ -19,17 +22,23 @@ packagePage($sections, $post);
 foreach(get_pages($args) as $page) {
     packagePage($sections, $page);
 }
-
 ?>
 
 <div class="container-fluid home">
     <?php foreach($sections as $section): ?>
-        <?php list($page, $image) = $section; ?>
+        <?php list($page, $image, $bottom_field) = $section; ?>
         <section id="<?= $page->post_name ?>" class="<?= $page->post_name ?>" style="background-image: url('<?= $image; ?>')">
             <div class="ghost"></div>
             <div class="inner-content">
+                <?php
+                    var_dump($page);
+                ?>
                 <?= apply_filters('the_content', $page->post_content) ?>
             </div>
+            
+            <?php if (strlen($bottom_field)): ?> 
+                <h4 class="bottom-field"><?= $bottom_field ?></h4>
+            <?php endif; ?>
         </section>
     <?php endforeach; ?>
 </div>
