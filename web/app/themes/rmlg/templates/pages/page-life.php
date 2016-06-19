f7d0a38d3d168fe32e5293be5694c3e126806a04
Grant Tepper <grant.tepper@gmail.com>
2016-06-19 01:52:14 -0700

    updates styles and copy

--- web/app/themes/rmlg/templates/pages/page-life.php
+++ web/app/themes/rmlg/templates/pages/page-life.php
@@ -1,77 +1,61 @@
 <div>
     <div class="row">
-        <div class="col-lg-12 text-center">
-            <h2 class="section-heading">About</h2>
-            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-        </div>
-    </div>
-    <div class="row">
         <div class="col-lg-12">
             <ul class="timeline">
                 <li>
                     <div class="timeline-image">
-                        <img class="img-circle img-responsive" src="img/about/1.jpg" alt="">
+                        <img class="img-circle img-responsive" src="<?= get_stylesheet_directory_uri();?>/dist/images/mine2.jpg" alt="">
                     </div>
                     <div class="timeline-panel">
                         <div class="timeline-heading">
-                            <h4>2009-2011</h4>
+                            <h4>2009-2015</h4>
                             <h4 class="subheading">Our Humble Beginnings</h4>
                         </div>
                         <div class="timeline-body">
-                            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                            <p >Going by many names during this period.  We grew and gained much experience in all the realms of EVE Online.  From high-sec POS bashing - massinve industrial opperations - life in wormholes and 0.0 combat - we gained the experience we needed to build a better corporation.</p>
                         </div>
                     </div>
                 </li>
                 <li class="timeline-inverted">
                     <div class="timeline-image">
-                        <img class="img-circle img-responsive" src="img/about/2.jpg" alt="">
+                        <img class="img-circle img-responsive" src="<?= get_stylesheet_directory_uri();?>/dist/images/brigde2.jpg" alt="">
                     </div>
                     <div class="timeline-panel">
                         <div class="timeline-heading">
-                            <h4>March 2011</h4>
-                            <h4 class="subheading">An Agency is Born</h4>
+                            <h4>August 2015</h4>
+                            <h4 class="subheading">Rebirth</h4>
                         </div>
                         <div class="timeline-body">
-                            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                            <p >After returning from the game and calling to arms old friends, we reformed and shortly merged with an ally - Cerberus Military Group.  After fighting off a common foe we took an opportunity to join old friends in the Drone Lands during which time we grew to one of the top PVP corporations in the alliance and took part in one of the larger conflicts in recent EVE history.</p>
                         </div>
                     </div>
                 </li>
                 <li>
                     <div class="timeline-image">
-                        <img class="img-circle img-responsive" src="img/about/3.jpg" alt="">
+                        <img class="img-circle img-responsive" src="<?= get_stylesheet_directory_uri();?>/dist/images/holes2.jpg" alt="">
                     </div>
                     <div class="timeline-panel">
                         <div class="timeline-heading">
-                            <h4>December 2012</h4>
-                            <h4 class="subheading">Transition to Full Service</h4>
+                            <h4>Present</h4>
+                            <h4 class="subheading">A New Home</h4>
                         </div>
                         <div class="timeline-body">
-                            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                            <p >We have grown to become a well established independent military and industrial power - with this power came the need for change.  True indepedance is hard to find in EVE - we have sought out to find it in space we fondly call Valhalla. </p>
                         </div>
                     </div>
                 </li>
                 <li class="timeline-inverted">
                     <div class="timeline-image">
-                        <img class="img-circle img-responsive" src="img/about/4.jpg" alt="">
-                    </div>
-                    <div class="timeline-panel">
-                        <div class="timeline-heading">
-                            <h4>July 2014</h4>
-                            <h4 class="subheading">Phase Two Expansion</h4>
-                        </div>
-                        <div class="timeline-body">
-                            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                        </div>
-                    </div>
-                </li>
-                <li class="timeline-inverted">
-                    <div class="timeline-image">
-                        <h4>Be Part
-                            <br>Of Our
-                            <br>Story!</h4>
+                        <h4> 
+                            Become <br>Part of<br>
+                            the Story
+                        </h4>
                     </div>
                 </li>
             </ul>
+        </div>
+        <div class="col-lg-12">
+            <h4 class="text-center"><i>The future only holds what we imagine for it.</i></h4>
         </div>
     </div>
 </div>
